d964d77498b21ad006f981fa672148a58b9da60f
kimsuwa <suwakim0331@icloud.com>
2020-04-30 10:55:03 +0900

    add AA.c

---
+++ AA.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){ // 윈도우 체재는 main 함수를 찾아서 실행한다.
+	printf("Q1.print rectangle!\n");
+	printf("@@@@@\n@   @\n@   @\n@@@@@\n\n"); // printf는 stdio.h에 정의 되어있어 include 시켜줘야 함
+	
+	printf("Q2.print triangle!\n");
+	printf("   $\n  $$$\n $$$$$ \n$$$$$$$\n\n");
+
+	printf("Q3.print B!\n");
+	printf("BBBBBB\nB      B\nB      B\nBBBBBB\nB      B\nB      B\nBBBBBB\n");
+	return 0;
+	//ctrl + F5 : 실행
+}
