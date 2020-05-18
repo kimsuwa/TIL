859e003ef14e7a628144317269556a035adb1a69
kimsuwa <suwakim0331@icloud.com>
2020-05-18 19:45:38 +0900

    add FindNumberOfAnimals.cpp

---
+++ FindNumberOfAnimals.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+	int Total_Animal_Num, Total_Leg_Num;
+	int rabbit_num=0, hen_num=0;
+
+	printf("Please Enter the total number of rabbits and hens:");
+	scanf_s("%d", &Total_Animal_Num);
+	printf("\nPlease Enter the total leg number of rabbits and hens:");
+	scanf_s("%d", &Total_Leg_Num);
+
+	//rabbit_num = -Total_Animal_Num + 0.5 * Total_Leg_Num;
+	//hen_num = 2*Total_Animal_Num - 0.5 * Total_Leg_Num;
+
+	for (int i = 1; i <= Total_Animal_Num; i++) {
+		for (int j = 1; j <= Total_Animal_Num; j++) {
+			if ((i + j) == Total_Animal_Num && (4*i+2*j) == Total_Leg_Num) {
+				rabbit_num = i;
+				hen_num = j;
+			}
+		}
+	}
+
+	printf("\n%d %d", rabbit_num, hen_num);
+}
